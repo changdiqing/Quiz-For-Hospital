eae0dd01742997205eb8178a17da41d2a2b6c390
changdiqing <changdiqing@live.cn>
2019-12-30 12:25:51 +0100

    build  locally and push for deployment

---
+++ client/build/precache-manifest.8623e66aea742f78bddab9b991e5385e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2ab6c4ca7e9cf819dd9603d2393d15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff4927ed2068c1dc1815",
+    "url": "/static/css/main.c3dfbbad.chunk.css"
+  },
+  {
+    "revision": "46f26f245424637f3a27",
+    "url": "/static/js/2.d61c023e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d61c023e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff4927ed2068c1dc1815",
+    "url": "/static/js/main.3e58c408.chunk.js"
+  },
+  {
+    "revision": "fdcdab7d1a93c099f76e",
+    "url": "/static/js/runtime-main.980a9437.js"
+  },
+  {
+    "revision": "6f8bf80284cbb8b753c04439040ef17a",
+    "url": "/static/media/icon.6f8bf802.png"
+  }
+]);
