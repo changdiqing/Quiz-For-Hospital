ee7d036afbe1e5de9b42a3abc0b7596bfc77ff0a
daniwano <daniel.zehe1@gmx.de>
2020-01-21 10:45:30 +0100

    before merge

---
+++ client/build/precache-manifest.0b59e125cdabff4e7006bfb9155f3baf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adc5faedf4b59482cf444836d6ba096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10239a17ed9c5c8f8f5f",
+    "url": "/static/css/2.35737f5e.chunk.css"
+  },
+  {
+    "revision": "5899b5e728fe79441ae4",
+    "url": "/static/css/main.fd97558f.chunk.css"
+  },
+  {
+    "revision": "10239a17ed9c5c8f8f5f",
+    "url": "/static/js/2.c248542d.chunk.js"
+  },
+  {
+    "revision": "77d9a5a8d0635e146e324553e360082f",
+    "url": "/static/js/2.c248542d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5899b5e728fe79441ae4",
+    "url": "/static/js/main.7a00590b.chunk.js"
+  },
+  {
+    "revision": "fdcdab7d1a93c099f76e",
+    "url": "/static/js/runtime-main.980a9437.js"
+  },
+  {
+    "revision": "6f8bf80284cbb8b753c04439040ef17a",
+    "url": "/static/media/icon.6f8bf802.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c254851d925ddd99dde14de6b970198.js"
+  "/precache-manifest.0b59e125cdabff4e7006bfb9155f3baf.js"
 );
 
 self.addEventListener('message', (event) => {
